246a3d36818a8083821b32b46c9af20cc4d3252f
Jesse-Driskill <jesse@driskill.com>
2024-03-21 00:49:27 -0700

    Adds acceleration

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.body.style.overflow = 'hidden';
     
-    let b = {x: 300, y: 0, w: 30, h: 30, vX: 0, vY: 0};
+    let terminalVelocity = 30;
+    let maximumAcceleration = 10;
+
+    let b = {x: 300, y: 0, w: 30, h: 30, vX: 0, vY: 0, vA: 0};
     let obstacles = [];
     let gravity = 1;
     let counter = 0;
@@ -22,21 +25,49 @@ document.addEventListener("DOMContentLoaded", () => {
     
     
     function startGame() {
+        
+        function youLose() {
+            console.log("You lose!");
+            let youLose = document.createElement("h1");
+            youLose.innerHTML = "YOU LOSE!";
+            youLose.style.position = 'absolute';
+            youLose.style.top = "50%";
+            youLose.style.left = "50%";
+            document.body.appendChild(youLose);
+            clearInterval(loopInterval);
+        }
         document.addEventListener("click", () => {
-    
-            if (b.vY > -26) {
-    
-                b.vY -= 15;
+            
+            if (b.vA > -100) {
+                b.vA -= 50;
             }
-        });
-        setInterval(() => {
+            
 
+        });
+
+
+        var loopInterval = setInterval(() => {
+
+            //console.log(b.vA)
             ctx.clearRect(b.x,b.y,b.w,b.h);
 
-            if (b.vY < 2) {
-                b.vY += gravity;
+            //bird physics
+
+            
+            b.vA += gravity;
+            b.vY += b.vA;
+            if (b.vY > 6) {
+                b.vY = 6;
+            } else if (b.vY < -8) {
+                b.vY = -8;
             }
             b.y += b.vY;
+            
+            
+            
+
+            //bird physics
+
             
             ctx.fillStyle = "yellow";
             ctx.fillRect(b.x,b.y,b.w,b.h);
@@ -48,9 +79,26 @@ document.addEventListener("DOMContentLoaded", () => {
                 ctx.fillStyle = "#32CD32";
                 obstacle.x -= 2;
                 ctx.fillRect(obstacle.x, obstacle.y, 80, obstacle.h);
+
+                //collision checks
+                if (obstacle.x === b.x + 30) {
+                    console.log("UJIHDEFHUIUIHFEWNJKSDF")
+
+                    if (obstacle.type === "bottomObject") {
+                        if (obstacle.y < b.y) {
+                            console.log("LOSS DUE TO BOTTOM COLLISION");
+                            youLose();
+                        }
+                    } else if (obstacle.type === "topObject") {
+                        if (obstacle.y + obstacle.h > b.y ) {
+                            console.log("LOSS DUE TO TOP COLLISION");
+                            youLose();
+                        }
+                    }
+                }
             }
 
-            if (counter > 1300) {
+            if (counter > 1850) {
                 generateTopObject();
                 generateBottomObject();
                 counter = 0
@@ -58,30 +106,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
         
 
-            counter += 3;
+            counter += 11;
 
-            if (obstacles[0].x < -100) {
-                obstacles.shift();
-                console.log("shifted!");
-            }
-            if (obstacles[0].x < -100) {
-                obstacles.shift();
-                console.log("shifted!");
+            if (obstacles.length > 0) {
+                if (obstacles[0].x < -100) {
+                    obstacles.shift();
+                    //console.log("shifted!");
+                }
+                if (obstacles[0].x < -100) {
+                    obstacles.shift();
+                    //console.log("shifted!");
+                }
             }
 
-        }, 1);
+            if (b.y > 1100) {
+                youLose();
+            }
+
+            //console.log(obstacles[0].x);
+            //console.log(b.x)
+
+        }, 20);
 
     }
 
     startGame();
 
+
+    
+
     function generateTopObject() {
-        obstacles.push({x: screenWidth, y: 0, h: Math.max(Math.floor(Math.random() * 450), 200 + Math.floor(Math.random() * 100))});
+        obstacles.push({type: "topObject", x: screenWidth, y: 0, h: Math.max(Math.floor(Math.random() * 450), 200 + Math.floor(Math.random() * 100))});
     }
 
     function generateBottomObject() {
         let topleft = screenHeight - Math.max(Math.floor(Math.random() * 450), 200 + Math.floor(Math.random() * 100));
-        obstacles.push({x: screenWidth, y: topleft, h: screenHeight + 400});
+        obstacles.push({type: "bottomObject", x: screenWidth, y: topleft, h: screenHeight + 400});
     }
 
 })
